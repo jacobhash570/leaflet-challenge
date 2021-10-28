2e241ec1e5671ac5079f81bd7a8ee849fa8164ce
Jacob <jacob.hash570@gmail.com>
2021-10-28 11:36:25 -0400

    Circles markers added.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -22,13 +22,58 @@ streetmap.addTo(myMap);
 //GeoJSON URL
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 d3.json(queryUrl, function(data) {
+
+    // Styling for circles
+    function mapStyle(feature) {
+        return {
+          opacity: 2,
+          fillOpacity: 2,
+          fillColor: mapColor(feature.properties.mag),
+          radius: mapRadius(feature.properties.mag),
+          weight: .75
+        };
+      }
+
+      // Stying for circles based on magnitude
+      function mapColor(mag) {
+        switch (true) {
+          case mag > 5:
+            return "#ea2c2c";
+          case mag > 4:
+            return "#eaa92c";
+          case mag > 3:
+            return "#d5ea2c";
+          case mag > 2:
+            return "#92ea2c";
+          case mag > 1:
+            return "#2ceabf";
+          default:
+            return "#2c99ea";
+        }
+      }
+      
+      // Stying for circle radius
+      function mapRadius(mag) {
+        if (mag === 0) {
+          return 1;
+        }
     
+        return mag * 5;
+      }
+    
+    //Add to map
     L.geoJson(data, {
+
+        pointToLayer: function(feature, lat_lng) {
+            return L.circleMarker(lat_lng);
+        },
+      
+        style: mapStyle,
   
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("<br>Location: " + feature.properties.place);
+        onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
   
       }
-}).addTo(myMap);
+    }).addTo(myMap);
 
 })
