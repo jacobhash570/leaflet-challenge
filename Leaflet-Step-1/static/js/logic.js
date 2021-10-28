5536da9e0e559e01a8eeedd3f12c2aa63833c94a
Jacob <jacob.hash570@gmail.com>
2021-10-28 10:30:04 -0400

    Map added.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,9 +1,20 @@
-// Get DATA
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-// GET 
-d3.json(queryUrl).then(data => {
-
-      createFeatures(data.features);
-    
+// Inititalize Map
+var myMap = L.map("map", {
+    center: [
+        37.09, -95.71
+    ],
+    zoom: 5,
+      
     });
+
+//Define and Add
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 10,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+});
+
+streetmap.addTo(myMap);
